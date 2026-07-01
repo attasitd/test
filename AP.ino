75b5431f7032a8425c9ffd64e153e22ff5dae466
Attasit Datsong <attasitd@hotmail.com>
2026-07-01 14:29:20 +0700

    Initialize ESP32 in AP mode with WiFi credentials

---
+++ AP.ino
@@ -0,0 +1,11 @@
+#include<WiFi.h>
+#define WIFI_AP_NAME "ESP32 AP mode"
+#define WIFI_AP_PASS 12345678
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(WIFI_AP_NAME, WIFI_AP_PASS);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP());
+}
+void loop() { }
